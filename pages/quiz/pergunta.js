7bf4e15e9bf722e712addf8af15fd7394a156f54
Gustavo Garcia <guga260608@gmail.com>
2024-09-05 08:51:37 -0300

    tá dando erro no quiz, na variável urlDados

--- pages/quiz/pergunta.js
+++ pages/quiz/pergunta.js
@@ -1,3 +1,10 @@
+let quiz = {}
+let pontos = 0
+let contador = 1
+let resposta = ""
+let idInputResposta = ""
+let respostaCorretaId = ""
+
 function selectOption(selectedButton) {
     const options = document.querySelectorAll('.option');
     options.forEach(option => option.classList.remove('selected'));
@@ -16,3 +23,90 @@ function tirarPopup() {
 function redirecionar() {
     window.location.href = "../quiz.html";
 }
+
+async function buscarPerguntas() {
+    const urlDados = "./pergunta.json"
+
+    await fetch(urlDados).then(resposta => resposta.json()).then(dados => {})
+}
+
+function montarPergunta() {
+    const main = document.querySelector(".bloco")
+    main.innerHTML = `
+        <div class="header">
+            <button class="sair" onclick="mostrarPopup()">SAIR</button>
+            <span>Pergunta ${contador}/10</span>
+        </div>
+        <div class="question">
+            <p>${urlDados.perguntas[contador-1].pergunta}</p>
+            <div class="options">
+                <button class="option" onclick="selectOption(this)">300 km/s</button>
+                <button class="option" onclick="selectOption(this)">300.000 km/s</button>
+                <button class="option" onclick="selectOption(this)">300.000 km/h</button>
+                <button class="option" onclick="selectOption(this)">3.000 km/s</button>
+            </div>
+        </div>
+        <button class="verify-button">Verificar</button>
+    `}
+
+function guardarResposta(evento) {
+    resposta = evento.target.value
+    idInputResposta = evento.target.id
+
+    const botaoEnviar = document.querySelector(".verify-button")
+    botaoEnviar.addEventListener("click", validarResposta)
+}
+
+function validarResposta() {
+    const botaoEnviar = document.querySelector(".verify-button")
+    botaoEnviar.innerText = "Próxima"
+    botaoEnviar.removeEventListener("click", validarResposta)
+
+    if (pergunta === 10) {
+        botaoEnviar.innerText = "Finalizar"
+        botaoEnviar.addEventListener("click", finalizar)
+    } else {
+        botaoEnviar.addEventListener("click", proximaPergunta)
+    }
+    
+    if (resposta === urlDados.perguntas[pergunta-1].respostaCorresta) {
+        document.querySelector(`label[for='${idInputResposta}']`).setAttribute("id", "correta")
+        pontos = pontos + 1
+    } else {
+        document.querySelector(`label[for='${idInputResposta}']`).setAttribute("id", "errada")
+        document.querySelector(`label[for='${respostaCorretaId}']`).setAttribute("id", "correta")
+    }
+    
+    pergunta = pergunta + 1
+}
+    
+function finalizar() {
+    localStorage.setItem("pontos", pontos)
+    
+    window.location.href = "./resultado.html"
+}
+    
+function proximaPergunta() {
+    montarPergunta()
+    adicionarEventoInputs()
+}
+    
+function adicionarEventoInputs() {
+    const inputsResposta = document.querySelectorAll(".options button")
+    inputsResposta.forEach(input => {
+        input.addEventListener("click", guardarResposta)
+    
+        if (input.value === urlDados.perguntas[pergunta-1].respostaCorresta){
+            respostaCorretaId = input.id
+        }
+    })
+}
+    
+
+async function iniciar() {
+    await buscarPerguntas()
+    montarPergunta()
+    adicionarEventoInputs()
+}
+
+iniciar()
