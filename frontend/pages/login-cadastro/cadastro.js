64575f2292653d4b86f731ec673fc331090979e1
Gustavo Garcia <guga260608@gmail.com>
2024-11-27 16:11:57 -0300

    ajuste nas inputs do cadastro

--- frontend/pages/login-cadastro/cadastro.js
+++ frontend/pages/login-cadastro/cadastro.js
@@ -1,34 +1,40 @@
-async function register () {
-    const email = document.querySelector("#email").value
-    const usuario = document.querySelector("#username").value
-    const senha = document.querySelector("#password").value
+async function register() {
+    const email = document.querySelector("#email").value;
+    const usuario = document.querySelector("#username").value;
+    const senha = document.querySelector("#password").value;
+
+    // Verifica se os campos não estão vazios
+    if (!email || !usuario || !senha) {
+        alert("Por favor, preencha todos os campos.");
+        return;  // Não continua se algum campo estiver vazio
+    }
 
     const user = {
         email,
         usuario,
         senha
-    }
+    };
 
     const response = await fetch("https://backend-sideralis.vercel.app/register", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
-        body: JSON.stringify({user})
-    }).then(response => response.json())
+        body: JSON.stringify({ user })
+    }).then(response => response.json());
 
-    alert(response.message)
+    alert(response.message);
 
-    if(response.userExists) {
-        window.location.reload()
-        return
+    if (response.userExists) {
+        window.location.reload();
+        return;
     }
 
-    window.location.href = "./login.html"
+    window.location.href = "./login.html";
 }
 
-const button = document.querySelector("form button")
+const button = document.querySelector("form button");
 button.addEventListener("click", (event) => {
-    event.preventDefault()
-    register()
-})
+    event.preventDefault();
+    register();
+});
